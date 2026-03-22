591f22beff25217fdb1b18518528a5126f18d928
CC-LN4 <c.wienbeck@yahoo.de>
2026-03-22 14:49:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-console.log("Script geladen");
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Script geladen");
 
-// Elemente holen
-const abholAdresse = document.getElementById("abholAdresse");
-const radios = document.getElementsByName("uebergabe");
+    const abholAdresse = document.getElementById("abholAdresse");
+    const radios = document.getElementsByName("uebergabe");
 
-// Für jedes Radio-Button eine Änderung überwachen
-radios.forEach(radio => {
-    radio.addEventListener("change", function() {
-        if (this.value === "abholung") {
-            abholAdresse.style.display = "block";
-        } else {
-            abholAdresse.style.display = "none";
-        }
+    radios.forEach(radio => {
+        radio.addEventListener("change", function() {
+            if (this.value === "abholung") {
+                abholAdresse.style.display = "block";
+            } else {
+                abholAdresse.style.display = "none";
+            }
+        });
     });
 });
 
