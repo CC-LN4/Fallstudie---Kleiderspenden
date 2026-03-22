d85f9995cb86f5b084c2bf3bdeaf8ad5f1f2b144
CC-LN4 <c.wienbeck@yahoo.de>
2026-03-22 14:31:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Script geladen");
+// Elemente holen
+const abholAdresse = document.getElementById("abholAdresse");
+const radios = document.getElementsByName("uebergabe");
+
+// Für jedes Radio-Button eine Änderung überwachen
+radios.forEach(radio => {
+    radio.addEventListener("change", function() {
+        if (this.value === "abholung") {
+            abholAdresse.style.display = "block";
+        } else {
+            abholAdresse.style.display = "none";
+        }
+    });
+});
